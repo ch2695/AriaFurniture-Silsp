b1e9f2ff5d28b9abc99badb7cd235fb19cc40e14
hojun <chhj1993@naver.com>
2021-04-17 17:58:04 +0900

    아리아퍼니처 메인페이지 실습

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(function(){
+
+/*    $("#category a").mouseover(function(){
+        $("#subMenuBG").stop().slideDown(300);
+    });
+
+	$("#category a").mouseout(function(){
+        $("#subMenuBG").stop().slideUp(300);
+    });*/
+
+});
