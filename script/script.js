b8876bb28272482a951e25eb752300ee014f2358
hojun <chhj1993@naver.com>
2021-04-17 23:17:09 +0900

    아리아퍼니처 이미지 슬라이드 기능 추가

--- script/script.js
+++ script/script.js
@@ -1,11 +1,26 @@
 $(function(){
 
-/*    $("#category a").mouseover(function(){
+    $("#category a").mouseover(function(){
         $("#subMenuBG").stop().slideDown(300);
     });
 
 	$("#category a").mouseout(function(){
         $("#subMenuBG").stop().slideUp(300);
-    });*/
+    });
+
+	/* 이미지 슬라이드 */
+	setInterval(fnSlide, 9000);
+
+	function fnSlide() {
+		$("#shuttleFrame").animate({
+			"margin-left": "-1160px"
+			},
+			1000,
+			function(){
+				$("#shuttleFrame a:first-child").insertAfter("#shuttleFrame a:last-child");
+				$("#shuttleFrame").css({"margin-left": "0"});
+		});
+	}
+	/* 이미지 슬라이드 */
 
 });
